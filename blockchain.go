df8c542ace57f3e2019fbb8c236e54ccb29c4c64
Ram Goli <ramsgoli@gmail.com>
2018-06-24 17:49:53 -0700

    add simple POW algorithm

--- blockchain.go
+++ blockchain.go
@@ -1,6 +1,8 @@
 package blockchain
 
 import (
+	"crypto/sha256"
+	"fmt"
 	"time"
 )
 
@@ -48,6 +50,11 @@ func (bc *Blockchain) NewBlock(proof int, previousHash string) *Block {
 	return newBlock
 }
 
+// LastBlock returns the last block in the chain
+func (bc *Blockchain) LastBlock() *Block {
+	return bc.Chain[len(bc.Chain)-1]
+}
+
 // NewTransaction creates a new transaction in the chain
 func (bc *Blockchain) NewTransaction(sender string, receiver string, amount int) int {
 	transaction := &Transaction{sender, receiver, amount}
@@ -55,4 +62,26 @@ func (bc *Blockchain) NewTransaction(sender string, receiver string, amount int)
 
 	// return index of block that will hold this transaction
 	return bc.Chain[len(bc.Chain)-1].Index + 1
+}
+
+/*
+	simple proof of work algorithm
+	we want to find a hash p such that when hashed with the previous hash,
+	we get four leading zeros (Bitcoin uses 18 leading zeros)
+	we pretend that the previous hash is the block header
+*/
+func (bc *Blockchain) proofOfWork(lastProof int) int {
+	p := 0
+	for !isValidProof(p, lastProof) {
+		p++
+	}
+
+	return p
+}
+
+func isValidProof(nonce int, lastProof int) bool {
+	proof := fmt.Sprintf("%d%d", nonce, lastProof)
+	byteHash := sha256.Sum256([]byte(proof))
+	stringHash := fmt.Sprintf("%s", byteHash)
+	return stringHash[:4] == "0000"
 }
