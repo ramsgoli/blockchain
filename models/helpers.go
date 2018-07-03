5c5810a22bec4a971b4ecfe44b2288f957f0f369
Ram Goli <ramsgoli@gmail.com>
2018-07-02 22:21:38 -0700

    feat: add isValidChain function

--- models/helpers.go
+++ models/helpers.go
@@ -10,3 +10,24 @@ type mineResponse struct {
 	Transactions []Transaction `json:"transactions"`
 	PreviousHash string        `json:"previous_hash"`
 }
+
+func (bc *Blockchain) isValidChain() bool {
+	previousBlock := bc.Chain[0]
+	currentIndex := 1
+
+	for currentIndex < len(bc.Chain) {
+		block := bc.Chain[currentIndex]
+		if bc.hash(previousBlock) != block.PreviousHash {
+			return false
+		}
+
+		if !isValidProof(previousBlock.Proof, block.Proof) {
+			return false
+		}
+
+		previousBlock = block
+		currentIndex++
+	}
+
+	return true
+}
